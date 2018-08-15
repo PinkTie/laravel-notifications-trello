ed2f30c78ae27cd03a1a03698ea1e00f4f8bf721
Kevin Fairbanks <kevin@pinktietech.com>
2018-08-15 17:32:23 -0400

    Create TrelloChannel.php

---
+++ src/TrelloChannel.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace PinkTie\TrelloNotifications;
+
+use GuzzleHttp\Client;
+use Illuminate\Support\Arr;
+use PinkTie\TrelloNotifications\Exceptions\CouldNotSendNotification;
+use Illuminate\Notifications\Notification;
+use PinkTie\TrelloNotifications\Exceptions\InvalidConfiguration;
+
+class TrelloChannel
+{
+    const API_ENDPOINT = 'https://api.trello.com/1/cards/';
+
+    /** @var Client */
+    protected $client;
+
+    /** @param Client $client */
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * Send the given notification.
+     *
+     * @param mixed $notifiable
+     * @param \Illuminate\Notifications\Notification $notification
+     *
+     * @throws \PinkTie\TrelloNotifications\Exceptions\InvalidConfiguration
+     * @throws \PinkTie\TrelloNotifications\Exceptions\CouldNotSendNotification
+     */
+    public function send($notifiable, Notification $notification)
+    {
+        if (! $routing = collect($notifiable->routeNotificationFor('Trello'))) {
+            return;
+        }
+
+        $key = config('services.trello.key');
+
+        if (is_null($key)) {
+            throw InvalidConfiguration::configurationNotSet();
+        }
+
+        $trelloParameters = $notification->toTrello($notifiable)->toArray();
+
+        $response = $this->client->post(self::API_ENDPOINT.'?key='.$key.'&token='.$routing->get('token'), [
+            'form_params' => Arr::set($trelloParameters, 'idList', $routing->get('idList')),
+        ]);
+
+        if ($response->getStatusCode() !== 200) {
+            throw CouldNotSendNotification::serviceRespondedWithAnError($response);
+        }
+    }
+}
